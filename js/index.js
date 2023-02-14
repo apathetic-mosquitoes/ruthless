6fec6559889317267ee2baaa24b49ebddb86cd64
Nate Brown <nathan.d.brown@gmail.com>
2023-02-14 09:55:24 -0800

    eslint fix

--- js/index.js
+++ js/index.js
@@ -6,17 +6,15 @@ let apptForm = document.querySelector('#apptForm');
 let cancelForm = document.querySelector('#cancelForm');
 let contactLink = document.querySelector('#contact-us');
 let scheduleLink = document.querySelector('#schedule');
-let submitApptButton = document.querySelector("#submit-appt")
-let APPOINTMENT_ARRAY = []
 // constructor for appointments
-function Appointment (name, email, type, sessionStart, sessionEnd, message, date) {
-    this.name = name,
-    this.email = email,
-    this.type = type,
-    this.sessionStart = sessionStart,
-    this.sessionEnd = sessionEnd, 
-    this.message = message,
-    this.date = date
+function Appointment(name, email, type, sessionStart, sessionEnd, message, date) {
+  this.name = name,
+  this.email = email,
+  this.type = type,
+  this.sessionStart = sessionStart,
+  this.sessionEnd = sessionEnd,
+  this.message = message,
+  this.date = date;
 }
 
 //let contacts = [];
@@ -25,87 +23,75 @@ let appts = [];
 function handleScroll(event) {
   event.preventDefault();
   if (event.target.id === 'contact-us') {
-    contactForm.scrollIntoView({ behavior: "smooth" })
+    contactForm.scrollIntoView({ behavior: 'smooth' });
   }
   else if (event.target.id === 'schedule') {
-    apptForm.scrollIntoView({ behavior: "smooth" })
+    apptForm.scrollIntoView({ behavior: 'smooth' });
   }
 
 }
 
-// 
-function apptHandler(event){
-    event.preventDefault()
-
-    let newAppointment = new Appointment(
-        event.target.name.value,
-        event.target.email.value,
-        event.target.typeOfSession.value,
-        event.target.startTime.value,
-        event.target.endTime.value,
-        event.target.apptMessage.value,
-        event.target.date.value,
-    )
-    // DONE: get local storage if exists, parse, and append new appointment
-    let appointment_array = getLocalStorage('appointment_array')
-    
-    appointment_array.push(newAppointment)
-    
-    // stringify the appointment array
-    let strinfied_array = JSON.stringify(appointment_array)
-
-    // put back into local storage
-    localStorage.setItem("appointment_array", strinfied_array)
-
-    // TODO: clear form inputs after submit
-    apptForm.reset()
-
-
-    // TODO: display form submitted message
-    let successMessage = document.getElementById('successMessage')
-
-    console.log(successMessage)
-    successMessage.textContent = "Thanks, your appointment request has been scheduled!"
-
-    successMessage.style.display = 'block';
-    successMessage.style.color = 'green';
-    setTimeout(function(){
-        successMessage.style.display = 'none';
-    }, 2000)
-  }
-
-function handleScroll(event){
+//
+function apptHandler(event) {
   event.preventDefault();
-  if (event.target.id === 'contact-us') {
-    contactForm.scrollIntoView({behavior: "smooth"});
-  }
-  else if (event.target.id === 'schedule'){
-    apptForm.scrollIntoView({behavior: "smooth"});
-  }
 
+  let newAppointment = new Appointment(
+    event.target.name.value,
+    event.target.email.value,
+    event.target.typeOfSession.value,
+    event.target.startTime.value,
+    event.target.endTime.value,
+    event.target.apptMessage.value,
+    event.target.date.value);
+  // DONE: get local storage if exists, parse, and append new appointment
+  let appointment_array = getLocalStorage('appointment_array');
+
+  appointment_array.push(newAppointment);
+
+  // stringify the appointment array
+  let strinfied_array = JSON.stringify(appointment_array);
+
+  // put back into local storage
+  localStorage.setItem('appointment_array', strinfied_array);
+
+  // TODO: clear form inputs after submit
+  apptForm.reset();
+
+
+  // TODO: display form submitted message
+  let successMessage = document.getElementById('successMessage');
+
+  console.log(successMessage);
+  successMessage.textContent = 'Thanks, your appointment request has been scheduled!';
+
+  successMessage.style.display = 'block';
+  successMessage.style.color = 'green';
+  setTimeout(function () {
+    successMessage.style.display = 'none';
+  }, 2000);
 }
 
-function saveToLocalStorage(arr){
+function saveToLocalStorage(arr) {
   localStorage.setItem('appts', JSON.stringify(arr));
 }
 
-function handleCancel(event){
+function handleCancel(event) {
   event.preventDefault();
   let inArray = false;
-  for(let i in appts){
-    if(event.target.cancel.value === appts[i].email){
+  for (let i in appts) {
+    if (event.target.cancel.value === appts[i].email) {
       appts.splice(i, 1);
       localStorage.clear();
       saveToLocalStorage(appts);
       inArray = true;
     }
   }
-  if(inArray===true){
+  if (inArray === true) {
     let message = document.createElement('span');
     message.textContent = 'We have found your appointment and canceled it';
     cancelForm.appendChild(message);
   }
-  else{
+  else {
     let message = document.createElement('span');
     message.textContent = 'No appointment found with this email address';
     cancelForm.appendChild(message);
@@ -120,18 +106,18 @@ apptForm.addEventListener('submit', apptHandler);
 // returns data from local storage, if any
 // takes in a "key" argument
 function getLocalStorage(key) {
-    // check local storage for specified key
-    let dataFromStorage = localStorage.getItem(key)
+  // check local storage for specified key
+  let dataFromStorage = localStorage.getItem(key);
 
-    let arr = []
-    // if that key exists in local storage, parse data and update it
-    if (dataFromStorage) {
-        let parsedData = JSON.parse(dataFromStorage)
+  let arr = [];
+  // if that key exists in local storage, parse data and update it
+  if (dataFromStorage) {
+    let parsedData = JSON.parse(dataFromStorage);
 
-        arr.push(parsedData)
-        return arr
-    }
-    return arr
+    arr.push(parsedData);
+    return arr;
+  }
+  return arr;
 }
 
 
@@ -183,7 +169,7 @@ function checkStorage() {
     CONTACT_ARRAY = JSON.parse(checkLocalStorage);
     console.log(CONTACT_ARRAY);
     return CONTACT_ARRAY;
-  // if not, return empty array
+    // if not, return empty array
   } else {
     console.log('nothing in local storage');
     return CONTACT_ARRAY;
