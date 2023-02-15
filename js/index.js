2c0a394fe58e32c20dbdb47193ae77b7caf161d4
mirandalu2020 <yuanling1007@hotmail.com>
2023-02-15 15:01:45 -0800

    styling on h1 and buttons

--- js/index.js
+++ js/index.js
@@ -221,9 +221,9 @@ function checkStorage() {
 window.onscroll = function(){scrollFunction()};
 function scrollFunction(){
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector('h1').style.fontSize = '30px';
-  } else {
     document.querySelector('h1').style.fontSize = '4rem';
+  } else {
+    document.querySelector('h1').style.fontSize = '6rem';
   }
 }
 let url = window.location.href
