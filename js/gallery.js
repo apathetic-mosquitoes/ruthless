1a21c80fb388d98b7af866070b29226dc70c4a9d
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-02-15 10:13:01 -0800

    added h1 and animation

--- js/gallery.js
+++ js/gallery.js
@@ -32,3 +32,12 @@ function handleMaternityClick(event){
     portraitSection.classList.add('no-show');    
     weddingSection.classList.add('no-show');  
 };
+
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector('h1').style.fontSize = '30px';
+  } else {
+    document.querySelector('h1').style.fontSize = '4rem';
+  }
+}
--- js/index.js
+++ js/index.js
@@ -208,3 +208,13 @@ function checkStorage() {
     return CONTACT_ARRAY;
   }
 }
+
+// eslint-disable-next-line semi
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector('h1').style.fontSize = '30px';
+  } else {
+    document.querySelector('h1').style.fontSize = '4rem';
+  }
+}
